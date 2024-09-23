df4d21d2904ab683c5a2ca692aa5b9ea24593697
vanguard <vanguard09@naver.com>
2024-09-23 16:01:50 +0900

    개발환경 구축

---
+++ src/main/java/com/restapi/project_AI_diary_backend/ProjectAiDiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.project_AI_diary_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAiDiaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAiDiaryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/project_AI_diary_backend/ProjectAiDiaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.project_AI_diary_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAiDiaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
